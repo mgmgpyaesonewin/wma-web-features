29f9e8b31c5a7a04ee2871d5ada090f2914c5e56
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-04-16 19:59:37 +0630

    testing Hello World Component

--- src/components/atoms/HelloWorld.jsx
+++ src/components/atoms/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function HelloWorld() {
   return (
     <span className="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-      Hello World !!!
+      Hello World !!
     </span>
   );
 }
