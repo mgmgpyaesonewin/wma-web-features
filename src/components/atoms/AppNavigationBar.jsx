96f9e9dce73ec70974aab0110b9a3550d1e3da32
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-04-16 08:58:17 +0630

    fix: 🐛 remove fix navigation
    remove fixed class for navigation, requiring need to add extra margin is
    misunderstanding

--- src/components/atoms/AppNavigationBar.jsx
+++ src/components/atoms/AppNavigationBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function AppNavigationBar() {
   return (
-    <nav className="bg-amber-400 dark:bg-amber-500 fixed w-full z-20 top-0 left-0 border-b border-amber-200 dark:border-amber-600">
+    <nav className="bg-amber-400 dark:bg-amber-500 w-full z-20 top-0 left-0 border-b border-amber-200 dark:border-amber-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
           Wave Money JS SDK
