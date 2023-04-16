6843a8bc97294baa4e8e6fffafc1f17151af384a
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-04-16 08:59:13 +0630

    feat: 🎸 disable all routes except for device information

--- src/components/atoms/NavigationLink.jsx
+++ src/components/atoms/NavigationLink.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function NavLink({ children, to }) {
+function NavLink({ children, to, disabled }) {
+  const isDisabled = disabled ? 'pointer-events-none' : 'pointer-events-auto';
+
   return (
     <Link
-      className="text-white text-center bg-slate-400 hover:bg-amber-400 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
+      className={`text-white text-center bg-slate-400 hover:bg-amber-400 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 ${isDisabled}`}
       to={to}
     >
       {children}
@@ -16,6 +18,7 @@ function NavLink({ children, to }) {
 NavLink.propTypes = {
   children: PropTypes.node.isRequired,
   to: PropTypes.string.isRequired,
+  disabled: PropTypes.bool.isRequired,
 };
 
 export default NavLink;
--- src/components/molecules/NavLinksGroup.jsx
+++ src/components/molecules/NavLinksGroup.jsx
@@ -3,14 +3,31 @@ import NavLink from '../atoms/NavigationLink';
 
 function NavLinksGroup() {
   return (
-    <div className="flex flex-col justify-center mx-2">
-      <NavLink to="/camera">Camera</NavLink>
-      <NavLink to="/location">Location</NavLink>
-      <NavLink to="/contacts">Contacts</NavLink>
-      <NavLink to="/settings">Settings</NavLink>
-      <NavLink to="/permissions">Permissions</NavLink>
-      <NavLink to="/otp">Read OTP from SMS</NavLink>
-      <NavLink to="/biometrics">Biometrics</NavLink>
+    <div className="flex flex-col justify-center mx-2 mt-2">
+      <NavLink to="/device-information" disabled={false}>
+        Device Information
+      </NavLink>
+      <NavLink to="/camera" disabled>
+        Camera
+      </NavLink>
+      <NavLink to="/location" disabled>
+        Location
+      </NavLink>
+      <NavLink to="/contacts" disabled>
+        Contacts
+      </NavLink>
+      <NavLink to="/settings" disabled>
+        Settings
+      </NavLink>
+      <NavLink to="/permissions" disabled>
+        Permissions
+      </NavLink>
+      <NavLink to="/otp" disabled>
+        Read OTP from SMS
+      </NavLink>
+      <NavLink to="/biometrics" disabled>
+        Biometrics
+      </NavLink>
     </div>
   );
 }
