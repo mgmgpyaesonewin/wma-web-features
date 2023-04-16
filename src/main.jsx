077e91db10c27d115c0b23653657c89c9fd5adc0
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-04-16 08:56:06 +0630

    fix: 🐛 remove duplicate page header
    in main.jsx, header component is direclty used causing duplicate headers
    in html node

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
-import PageHeader from './components/molecules/PageHeader';
 import './assets/css/index.css';
 
 const router = createBrowserRouter([
@@ -14,7 +13,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PageHeader />
     <RouterProvider router={router} />
   </React.StrictMode>,
 );
